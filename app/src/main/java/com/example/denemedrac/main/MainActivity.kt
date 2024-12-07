f700ac64939d614e3deba06090a37f8d219faf7e
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-12-07 16:06:47 +0300

    edit comit

--- app/src/main/java/com/example/denemedrac/main/MainActivity.kt
+++ app/src/main/java/com/example/denemedrac/main/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.denemedrac.main
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
@@ -12,8 +14,6 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.denemedrac.R
-import com.example.denemedrac.begin.OnboardingActivity
-import com.example.denemedrac.begin.WelcomeMessageActivity
 import com.example.denemedrac.music.MusicViewActivity
 import com.example.denemedrac.settings.SettingsActivity
 import com.example.denemedrac.shopping.ShoppingViewActivity
@@ -22,8 +22,8 @@ import com.example.denemedrac.sleep.SleepModeActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var buttonBar: View
-    private lateinit var showButtonBarButton: Button
+    private lateinit var buttonBar: LinearLayout
+    private lateinit var hideButtonBar: Button
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var mainContainer: LinearLayout
 
@@ -33,24 +33,12 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize SharedPreferences
         sharedPreferences = getSharedPreferences("userInfo", MODE_PRIVATE)
-        val userName = sharedPreferences.getString("userName", null)
-
-        /*if (userName == null) {
-            startActivity(Intent(this, OnboardingActivity::class.java))
-            finish() // Close MainActivity
-        } else {
-            if (isUserInfoSaved()) {
-                // Go to WelcomeMessageActivity for the first time
-                startActivity(Intent(this, WelcomeMessageActivity::class.java))
-                isUserInfoSaved().also { sharedPreferences.edit().putBoolean("isUserInfoSaved", false).apply() }
-            }
-        }*/
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         mainContainer = findViewById(R.id.main_container)
         buttonBar = findViewById(R.id.button_bar)
-        showButtonBarButton = findViewById(R.id.hide_button_bar)
+        hideButtonBar = findViewById(R.id.hide_button_bar)
 
         setupButtonBar()
         setupActiveViews()
@@ -63,48 +51,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateLayoutForOrientation(orientation: Int) {
-        // Ekran yatay moda geçtiğinde butonların dikey sıralanması
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            mainContainer.orientation = LinearLayout.HORIZONTAL
+        // Her iki yönelimde de button bar'ı horizontal tut
+        buttonBar.orientation = LinearLayout.HORIZONTAL
 
-            // Button bar sağ tarafa yerleşecek şekilde hizalanıyor
-            val buttonBarParams = buttonBar.layoutParams as FrameLayout.LayoutParams
-            buttonBarParams.width = resources.getDimensionPixelSize(R.dimen.button_bar_landscape_width)
-            buttonBarParams.height = FrameLayout.LayoutParams.MATCH_PARENT
-            buttonBar.layoutParams = buttonBarParams
-
-            // button_bar'ın orientation'ını dikey yap
-            (buttonBar as LinearLayout).orientation = LinearLayout.VERTICAL
-
-            // Hide button'ın yönü de değişiyor
-            showButtonBarButton.rotation = 90f
-        } else {
-            mainContainer.orientation = LinearLayout.VERTICAL
-
-            // Button bar alt kısımda yatay kalıyor
-            val buttonBarParams = buttonBar.layoutParams as FrameLayout.LayoutParams
-            buttonBarParams.width = FrameLayout.LayoutParams.MATCH_PARENT
-            buttonBarParams.height = resources.getDimensionPixelSize(R.dimen.button_bar_portrait_height)
-            buttonBar.layoutParams = buttonBarParams
-
-            // button_bar'ın orientation'ını yatay yap
-            (buttonBar as LinearLayout).orientation = LinearLayout.HORIZONTAL
-
-            // Hide button'ın yönü de eski haline dönüyor
-            showButtonBarButton.rotation = 0f
-        }
+        // Button bar'ın bottom'da kalmasını sağla
+        val params = buttonBar.layoutParams as FrameLayout.LayoutParams
+        params.gravity = Gravity.BOTTOM
+        buttonBar.layoutParams = params
     }
+
     private fun setupButtonBar() {
-        val orientation = resources.configuration.orientation
-
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            // Yatay modda butonlar dikey olarak hizalanacak
-            (buttonBar as LinearLayout).orientation = LinearLayout.VERTICAL
-        } else {
-            // Dikey modda butonlar yatay olarak hizalanacak
-            (buttonBar as LinearLayout).orientation = LinearLayout.HORIZONTAL
-        }
-
         // Butonların tıklama olaylarını ayarla
         findViewById<Button>(R.id.btn_sleep_mode).setOnClickListener {
             startActivity(Intent(this, SleepModeActivity::class.java))
@@ -121,22 +77,18 @@ class MainActivity : AppCompatActivity() {
             })
         }
 
-        // showButtonBarButton'un tıklama olayı
-        findViewById<Button>(R.id.hide_button_bar).setOnClickListener {
-            toggleButtonBar()
-        }
-        showButtonBarButton.setOnClickListener {
+        // Hide button'un tıklama olayı
+        hideButtonBar.setOnClickListener {
             toggleButtonBar()
         }
 
-        // Diğer görünürlük ayarları
+        // Aktif view'ları toggle etme
         ActiveView.values().forEach { view ->
             findViewById<Button>(getButtonId(view)).setOnClickListener {
                 viewModel.toggleView(view)
             }
         }
     }
-
 
     private fun setupActiveViews() {
         viewModel.activeViews.observe(this) { activeViews ->
@@ -148,21 +100,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun toggleButtonBar() {
-        val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-        val rotateAnimation = if (buttonBar.visibility == View.VISIBLE) {
-            buttonBar.visibility = View.GONE
-            ObjectAnimator.ofFloat(showButtonBarButton, "rotation", if (isLandscape) 0f else 90f, if (isLandscape) 180f else -90f)
+        val isVisible = buttonBar.visibility == View.VISIBLE
+        val hideButton = findViewById<Button>(R.id.hide_button_bar)
+
+        // Translate animasyonu
+        val buttonBarTranslate = if (isVisible) {
+            ObjectAnimator.ofFloat(buttonBar, "translationY", 0f, buttonBar.height.toFloat())
         } else {
-            buttonBar.visibility = View.VISIBLE
-            ObjectAnimator.ofFloat(showButtonBarButton, "rotation", if (isLandscape) 180f else -90f, if (isLandscape) 0f else 90f)
+            ObjectAnimator.ofFloat(buttonBar, "translationY", buttonBar.height.toFloat(), 0f)
         }
 
-        rotateAnimation.duration = 300 // 300ms animation duration
-        rotateAnimation.start()
-    }
+        // Rotate animasyonu
+        val rotateAnimator = ObjectAnimator.ofFloat(
+            hideButton,
+            "rotation",
+            if (isVisible) 0f else 180f
+        )
 
-    private fun isUserInfoSaved(): Boolean {
-        return sharedPreferences.getBoolean("isUserInfoSaved", false)
+        buttonBarTranslate.duration = 300
+        rotateAnimator.duration = 300
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(buttonBarTranslate, rotateAnimator)
+
+        animatorSet.start()
+
+        // Görünürlüğü değiştir
+        buttonBar.visibility = if (isVisible) View.GONE else View.VISIBLE
+        hideButton.text = if (isVisible) "Show" else "Hide"
     }
 
     private fun getButtonId(view: ActiveView): Int {
